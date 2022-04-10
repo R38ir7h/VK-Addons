b9ce93b8e8266c81e91830291d4da650528cbddf
R38ir7h <102347701+R38ir7h@users.noreply.github.com>
2022-04-10 11:00:47 +0300

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+import { Handler, Storage } from './modules';
+// @ts-ignore
+import config from '../config.json' assert { type: 'json' };
+const { clusters } = config;
+const server = require('server');
+var now = new Date().toLocaleString();
+console.log(now, "Поздравляю! Сервер запущен.");
+console.log("Весия ноды:", process.version );
+console.log('[Бот - Феня] разработан по заказу сообщества Forsaken World | Rebirth.');
+console.log('[Бот - Феня] является собственностю сервера Forsaken World | Rebirth. Все права на "Бот - Феня" принадлежат его правообладателям.');
+console.log('[Бот - Феня] Запущен.');
+const handlers = await Promise.all(clusters.map((cluster, index) => (new Handler({
+    ...cluster,
+    index: index + 1
+})
+    .init())));
+const uniqueHandlers = handlers.reduce((handlers, handler) => {
+    const instanceIndex = handlers.findIndex(([{ prefix }]) => (prefix === handler.storage.prefix));
+    const hasInstance = instanceIndex !== -1;
+    const handlerGroupId = handler.cluster.vk.group_id;
+    if (!hasInstance) {
+        handlers.push([handler.storage, [handlerGroupId]]);
+    }
+    else {
+        handlers[instanceIndex][1].push(handlerGroupId);
+    }
+    return handlers;
+}, []);
+uniqueHandlers.forEach(async ([storage, groupIds]) => {
+    const keys = await storage.getKeys();
+    const outdatedKeys = keys.reduce((keys, key) => {
+        if (key.startsWith(Storage.PREFIX) &&
+            groupIds.findIndex((id) => key.includes(`-${id}-`)) === -1) {
+            keys.push(key);
+        }
+        return keys;
+    }, []);
+    await Promise.all(outdatedKeys.map((key) => (storage.set(key))));
+});
